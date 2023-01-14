7bf6cbb89117cbe70664273efcc1bbd729fde61a
Matt Perry <matt@mattperry.com>
2023-01-14 14:29:57 -0800

    Move non-license info in headers into README.md

---
+++ README.md
@@ -0,0 +1,48 @@
+# Big Daddy's Math Drills
+
+A cross-platform set of simple text-based drills in basic addition,
+subtraction, multiplication and division tables. It features programmed
+instruction, adaptive learning, and user profiles.
+
+## Files included
+
+  * LICENSE - The Gnu General Public License.
+  * Student.cpp - Implementation for Student class
+  * Student.h - Specification and implementation of the Student class.
+  * add.cpp - The addition module.
+  * bdmd.cpp - Contains main().
+  * div.cpp - The division module.
+  * makefile - The gcc makefile.
+  * mlt.cpp - The multiplication module.
+  * README.md - Instructions, notes, legal.
+  * sub.cpp - The subtraction module.
+  * vkit.cpp - Utilities.
+  * vkit.h - Header for vkit.cpp.
+
+## Revision History
+
+  * Version 1.0 released March 1998
+  * Version 1.2 released December 2003
+  * Version 2.0 released January 2008
+
+### Version notes
+
+This version has very little in the way of input validation and error checking
+and uses more memory and disk space than it would if it were modified so that
+it only created and saved templates needed for the current level of the user,
+but it is feature complete. It was compiled with gcc and tested under Suse
+Linux 10.3 and compiled with MS Visual Studio and tested under WinXP.
+
+## Home Page
+
+http://www.dicarlolaw.com/shareware.html
+
+## License
+
+GNU General Public License v3.0 or later
+
+See COPYING to see the full text.
+
+## Author Information
+
+Copyright 1998, 2003, 2007 Vincent DiCarlo
--- Student.cpp
+++ Student.cpp
@@ -1,67 +1,18 @@
 // Big Daddy's Math Drills
-// Version 2.0 released January 2008
+// Copyright (C) 1998, 2003, 2007 Vincent DiCarlo
 //
-// Big Daddy's Math Drills Copyright 1998, 2003, 2007 Vincent DiCarlo
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// Files inluded in the sourcecode distribution:
-// 		add.cpp - The the addition module.
-//	 	bdmd.cpp - Contains main().
-//		copying.txt - The Gnu general public license.
-// 		div.cpp - The division module.
-// 		makefile - The gcc makefile.
-// 		mlt.cpp - The multiplication module.
-// 		readme - Instructions, notes, legal.
-// 		Student.cpp - Implementation for Student class
-// 		Student.h - Specificiation and implementation
-// 				of the Student class.
-// 		sub.cpp - The subtraction module.
-// 		vkit.cpp - Utilities.
-// 		vkit.h - Header for vkit.cpp.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// 	Binaries released:
-//		bdmd - Linux executable.
-//		bdmd.exe - MS Windows executable.
-//
-// Revision History:
-// 		Version 1.0 released March 1998
-// 		Version 1.2 released December 2003
-//
-// 	Version notes:
-// 		This version has very little in the way
-// 		of input validation and error checking
-// 		and uses more memory and disk space
-// 		than it would if it were modified so
-// 		that it only created and saved templates
-// 		needed for the current level of the user,
-// 		but it is feature complete.  It was
-// 		compiled with gcc and tested under Suse 
-// 		Linux 10.3 and compiled with MS Visual
-// 		Studio and tested under WinXP.
-//
-//	License:
-//		This file, and all the other files in this
-//		distribution as described above, including 
-//		the binaries released, are part of Big 
-//		Daddy's Math Drills.  Big Daddy's Math Drills 
-//		is free software: you can redistribute it 
-//		and/or modify it under the terms of the GNU 
-//		General Public License as published by the 
-//		Free Software Foundation, either version 3
-//		of the License, or (at your option) any 
-//		later version.
-//
-//		Big Daddy's Math Drills is distributed
-//		in the hope that it will be useful,	but 
-//		WITHOUT ANY WARRANTY; without even the 
-//		implied warranty of MERCHANTABILITY or 
-//		FITNESS FOR A PARTICULAR PURPOSE.  See 
-//		the GNU General Public License for more 
-//		details.
-//
-//		You should have received a copy of the GNU
-//		General Public License along with Big 
-//		Daddy's Math Drills.  If not, see 
-//		<http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <fstream>
 #include "Student.h"
